2ff91ba8c4c3d585f8b9ee82e39f82cce7d278c3
lazzzykim <33sunghyeon@gmail.com>
2024-05-07 16:46:33 +0900

    숫자 비교하기

---
+++ src/level_0/Compare.java
@@ -0,0 +1,15 @@
+package level_0;
+// 숫자 비교하기
+public class Compare {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int num1, int num2) {
+        if (num1 == num2) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
