d62ff3e2516645768c67356645d153a16b401278
SungHyeon Kim <151768821+lazzzykim@users.noreply.github.com>
2024-05-31 18:27:57 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n) {
+        int count = (n + 1) / 2;
+
+        int[] answer = new int[count];
+
+        int index = 0;
+        for (int i = 1; i <= n; i += 2) {
+            answer[index++] = i;
+        }
+        
+        return answer;
+    }
+}
